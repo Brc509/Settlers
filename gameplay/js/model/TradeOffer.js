ab1fe742a4e4ea3d79dfd50de3b8556bc9d22a84
Braden Condie <Brc509@gmail.com>
2014-02-04 19:03:42 -0700

    TradeOffer and TurnTracker for Cole

--- gameplay/js/model/TradeOffer.js
+++ gameplay/js/model/TradeOffer.js
@@ -2,21 +2,21 @@
 The Trade Offer Class 
 <pre>
 Domain:
-            sender: unique id number of the sender
-            receiver: unique id number of the receiver
-			offer: a ResourceList containing items in offer
-            
-        Invariants:
-            INVARIANT: ownerID exists
-			INVARIANT: receiver exists
-         
-        Constructor Specification:
-            PRE: sender is a player's ID
-            PRE: receiver is a player's ID
-			PRE: offer ResourceList is not empty
-            POST: getSender() == sender
-            POST: getReceiver() == receiver
-			POST: getOffer() == offer
+    sender: unique id number of the sender
+    receiver: unique id number of the receiver
+	offer: a ResourceList containing items in offer
+        
+    Invariants:
+        INVARIANT: ownerID exists
+		INVARIANT: receiver exists
+     
+    Constructor Specification:
+        PRE: sender is a player's ID
+        PRE: receiver is a player's ID
+		PRE: offer ResourceList is not empty
+        POST: getSender() == sender
+        POST: getReceiver() == receiver
+		POST: getOffer() == offer
 </pre>
 @class TradeOffer 
 @constructor
@@ -24,10 +24,10 @@ Domain:
 @param {:receiver} (int)The ID of the receiver.
 @param {:offer} (ResourceList)The list containing items in offer.
 */
-function TradeOffer(sender, receiver, offer) {
-    this.sender = sender;
-    this.receiver = receiver;
-	this.offer = offer;
+function TradeOffer(newTradeOffer) {
+    this.sender = newTradeOffer.sender;
+    this.receiver = newTradeOffer.receiver;
+	this.offer = newTradeOffer.offer;
 }
 
 /**
--- gameplay/js/model/TurnTracker.js
+++ gameplay/js/model/TurnTracker.js
@@ -18,9 +18,9 @@ Domain:
 @param {:currentTurn} (int)The ID of the player whose turn it is.
 @param {:status} (String)The status of the player whose turn it is.
 */
-function TurnTracker(currentTurn, status) {
-    this.currentTurn = currentTurn;
-    this.status = status;
+function TurnTracker(newTurnTracker) {
+    this.currentTurn = newTurnTracker.currentTurn;
+    this.status = newTurntracker.status;
 }
 
 /**
@@ -36,11 +36,42 @@ TurnTracker.prototype.getCurrentTurn = function() {
 
 /**
 <pre>
-A currentTurn player needs to have a status defined
+    PRE: A currentTurn player needs to have a status defined
 </pre>
 @method getStatus
 @return {String} The status of the player whose turn it is.
 */
 TurnTracker.prototype.getStatus = function() {
     return this.status;
+}
+
+/**
+    <pre>
+        PRE: queryStatus is a valid int with value in the range of 0-3
+        POST: Correctly returned true if the two statuses match up
+    </pre>
+    @method isStatus
+    @return {boolean} Whether the current status matches queryStatus
+*/
+TurnTracker.prototype.isStatus = function(queryStatus) {
+
+    if (this.status == queryStatus)
+        return true;
+    return false;
+}
+
+/**
+    <pre>
+        PRE: queryCurrentTurn is a valid string
+        POST: Correctly returned true if the two currentTurn strings match up
+    </pre>
+    @method isCurrentTurn
+    @return {boolean} Whether the "currentTurn" String matches the parameter "queryCurrentTurn"
+*/
+
+TurnTracker.prototype.isCurrentTurn = function(queryCurrentTurn) {
+
+    if (this.currentTurn = queryCurrentTurn)
+        return true;
+    return false;
 }

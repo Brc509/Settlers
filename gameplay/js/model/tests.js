062eb658f0491244189fea781b3d3f199b117d96
Skyler Mitchell <skylrmitchll@gmail.com>
2014-02-05 17:27:35 -0700

    moved phase0 out of the .gitignore folder.   Cole and Braden were ok with using the current copy of phase0 in the repository.

--- gameplay/js/model/tests.js
+++ gameplay/js/model/tests.js
@@ -3,3 +3,62 @@ test( "hello test", function() {
 
 	ok( 1 == "1", "Passed!" );
 });
+
+test( "sendChat", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+test( "rollNumber", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+// BUYDEVCARD, FINISHTURN, MONUMENT
+// ONLY REQUIRE MOVE TYPE AND PLAYER INDEX
+test( "Moves Simple", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+
+
+test( "Year_of_Plenty", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+test( "Soldier", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+test( "Monopoly", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+
+test( "Moves Build Tests", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+test( "Moves Trade Test", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+test( "discardCards", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+

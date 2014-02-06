01b6eb124ece474fe7c19aae7074acfab0a4e9df
Skyler Mitchell <skylrmitchll@gmail.com>
2014-02-05 17:33:05 -0700

    Stubbed the QUnit tests out.

--- gameplay/js/model/tests.js
+++ gameplay/js/model/tests.js
@@ -4,6 +4,29 @@ test( "hello test", function() {
 	ok( 1 == "1", "Passed!" );
 });
 
+// BUYDEVCARD, FINISHTURN, MONUMENT
+// ONLY REQUIRE MOVE TYPE AND PLAYER INDEX
+test( "Moves Simple", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+//BUILD ROAD, SETTLEMENT, CITY, ROAD_BUILDING
+test( "Moves Build Tests", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+//OFFER AND ACCEPT TRADE
+test( "Moves Trade Test", function() {
+	var clientModel = new catan.models.ClientModel(0);
+
+	ok( 1 == "1", "Passed!" );
+});
+
+
 test( "sendChat", function() {
 	var clientModel = new catan.models.ClientModel(0);
 
@@ -15,16 +38,6 @@ test( "rollNumber", function() {
 
 	ok( 1 == "1", "Passed!" );
 });
-
-// BUYDEVCARD, FINISHTURN, MONUMENT
-// ONLY REQUIRE MOVE TYPE AND PLAYER INDEX
-test( "Moves Simple", function() {
-	var clientModel = new catan.models.ClientModel(0);
-
-	ok( 1 == "1", "Passed!" );
-});
-
-
 
 test( "Year_of_Plenty", function() {
 	var clientModel = new catan.models.ClientModel(0);
@@ -38,19 +51,6 @@ test( "Soldier", function() {
 	ok( 1 == "1", "Passed!" );
 });
 test( "Monopoly", function() {
-	var clientModel = new catan.models.ClientModel(0);
-
-	ok( 1 == "1", "Passed!" );
-});
-
-
-test( "Moves Build Tests", function() {
-	var clientModel = new catan.models.ClientModel(0);
-
-	ok( 1 == "1", "Passed!" );
-});
-
-test( "Moves Trade Test", function() {
 	var clientModel = new catan.models.ClientModel(0);
 
 	ok( 1 == "1", "Passed!" );

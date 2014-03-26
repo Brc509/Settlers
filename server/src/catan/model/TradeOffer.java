4cd8ce6c830e75bb54739c9a124f266080afe692
nikolashin <g.nikolashin@gmail.com>
2014-03-26 16:08:46 -0600

    more client model trade offer fixes

--- server/src/catan/model/TradeOffer.java
+++ server/src/catan/model/TradeOffer.java
@@ -4,11 +4,7 @@ public class TradeOffer {
 	
 	private int receiver = -1;
 	private int playerIndex = -1;
-	private int brick = 0;
-	private int ore = 0;
-	private int sheep = 0;
-	private int wheat = 0;
-	private int wood = 0;
+	Offer offer = new Offer();
 	String type = "";
 
 	public TradeOffer() {
@@ -38,46 +34,5 @@ public class TradeOffer {
 	public void setType(String type) {
 		this.type = type;
 	}
-
-	public int getBrick() {
-		return brick;
-	}
-
-	public void setBrick(int brick) {
-		this.brick = brick;
-	}
-
-	public int getOre() {
-		return ore;
-	}
-
-	public void setOre(int ore) {
-		this.ore = ore;
-	}
-
-	public int getSheep() {
-		return sheep;
-	}
-
-	public void setSheep(int sheep) {
-		this.sheep = sheep;
-	}
-
-	public int getWheat() {
-		return wheat;
-	}
-
-	public void setWheat(int wheat) {
-		this.wheat = wheat;
-	}
-
-	public int getWood() {
-		return wood;
-	}
-
-	public void setWood(int wood) {
-		this.wood = wood;
-	}
-	
 
 }

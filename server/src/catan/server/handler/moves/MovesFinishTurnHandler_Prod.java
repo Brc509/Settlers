8836cd240d6d94b3f8c02c06eb14c7b201c4f760
nikolashin <g.nikolashin@gmail.com>
2014-03-25 20:51:27 -0600

    some more progress

--- server/src/catan/server/handler/moves/MovesFinishTurnHandler_Prod.java
+++ server/src/catan/server/handler/moves/MovesFinishTurnHandler_Prod.java
@@ -2,6 +2,11 @@ package catan.server.handler.moves;
 
 import java.io.IOException;
 
+import catan.server.command.moves.MovesBuildSettlementCommand;
+import catan.server.command.moves.MovesFinishTurnCommand;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 
 public class MovesFinishTurnHandler_Prod implements MovesFinishTurnHandler {
@@ -9,5 +14,9 @@ public class MovesFinishTurnHandler_Prod implements MovesFinishTurnHandler {
 	@Override
 	public void handle(HttpExchange arg0) throws IOException {
 		// TODO Auto-generated method stub
+	    java.util.Scanner s = new java.util.Scanner(arg0.getRequestBody()).useDelimiter("\\A");
+	    Gson g = new Gson();
+	    JsonObject json = g.fromJson(s.hasNext() ? s.next() : "", JsonObject.class);		
+		MovesFinishTurnCommand test = new MovesFinishTurnCommand(arg0, json);
 	}
 }

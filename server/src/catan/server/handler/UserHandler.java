95b02d076bb00b56663c31b32d159f778d70a7a7
Skyler Mitchell <skylrmitchll@gmail.com>
2014-03-28 13:58:12 -0600

    started user handler

--- server/src/catan/server/handler/UserHandler.java
+++ server/src/catan/server/handler/UserHandler.java
@@ -8,7 +8,9 @@ import com.sun.net.httpserver.HttpHandler;
 public class UserHandler implements HttpHandler {
 
 	@Override
-	public void handle(HttpExchange arg0) throws IOException {
+	public void handle(HttpExchange exchange) throws IOException {
+		String endPoint = exchange.getRequestURI().getPath();
+		System.out.println(endPoint);
 		// TODO Auto-generated method stub
 
 	}

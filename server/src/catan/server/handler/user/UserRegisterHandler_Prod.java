e439901393067aef27e319c98abed9c7eb5bf252
Spencer Bench <spencer.bench@gmail.com>
2014-03-27 20:22:57 -0600

    Fixed bug #329. Turned out to be an off-by-one error when setting a new user's cookie.

--- server/src/catan/server/handler/user/UserRegisterHandler_Prod.java
+++ server/src/catan/server/handler/user/UserRegisterHandler_Prod.java
@@ -19,45 +19,44 @@ public class UserRegisterHandler_Prod implements UserRegisterHandler {
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		
+
 		RegisteredUsers rUsers = RegisteredUsers.get();
-		
+
 		if (Server.isDebugEnabled()) System.out.println("\n" + this.getClass().getSimpleName() + ":");
 		if (exchange.getRequestMethod().toUpperCase().equals("POST")) {
-			
+
 			InputStream headers = exchange.getRequestBody();
 			String registerInfor = HandlerUtils.inputStreamToString(headers);
-			Map<String,String> registerInfoMap = HandlerUtils.decodeQueryString(registerInfor);
-			
+			Map<String, String> registerInfoMap = HandlerUtils.decodeQueryString(registerInfor);
+
 			Boolean didFind = false;
-			
-			for(int i = 0; i < rUsers.getUsers().size(); i++){
+
+			for (int i = 0; i < rUsers.getUsers().size(); i++) {
 				String username = registerInfoMap.get("username");
-				
+
 				String uNameToCheck = rUsers.getUsers().get(i).getName();
-				
-				if(uNameToCheck.equals(username)){
+
+				if (uNameToCheck.equals(username)) {
 					didFind = true;
 					break;
-				}		
+				}
 			}
-			
-			if(!didFind){
-				RegisteredUser rUser = new RegisteredUser(registerInfoMap.get("username"),registerInfoMap.get("password"), rUsers.getUsers().size());
+
+			if (!didFind) {
+				RegisteredUser rUser = new RegisteredUser(registerInfoMap.get("username"), registerInfoMap.get("password"), rUsers.getUsers().size());
 				rUsers.addUser(rUser);
-			}else{
+			} else {
 				HandlerUtils.sendString(exchange, HttpURLConnection.HTTP_BAD_REQUEST, "Username already taken");
 			}
-			
+
 			if (Server.isDebugEnabled()) System.out.println("  /user/register");
-			
-			Cookie cookie = new Cookie(registerInfoMap.get("username"), registerInfoMap.get("password"), rUsers.getUsers().size());
+
+			Cookie cookie = new Cookie(registerInfoMap.get("username"), registerInfoMap.get("password"), rUsers.getUsers().size() - 1);
 			Gson gson = new Gson();
 			String cookieString = gson.toJson(cookie);
 			cookieString = URLEncoder.encode(cookieString, "UTF-8");
 			HandlerUtils.addCookie(exchange, "catan.user", cookieString);
 
-			
 			HandlerUtils.sendString(exchange, HttpURLConnection.HTTP_OK, "Success");
 		} else {
 			if (Server.isDebugEnabled()) System.out.println("  Bad request to /user/register.");

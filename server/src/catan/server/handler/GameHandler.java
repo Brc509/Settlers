8c7635dc9fa01bfdbbf404c9026e3e7f0842edb4
Skyler Mitchell <skylrmitchll@gmail.com>
2014-03-29 13:50:45 -0600

    gameHandler and moved check request method to hanlderUtils

--- server/src/catan/server/handler/GameHandler.java
+++ server/src/catan/server/handler/GameHandler.java
@@ -1,6 +1,7 @@
 package catan.server.handler;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import catan.server.Server;
 
@@ -17,10 +18,44 @@ public class GameHandler implements HttpHandler {
 		// Based on the request path, execute the appropriate operation
 		String requestMethod = exchange.getRequestMethod().toUpperCase();
 		String endpoint = exchange.getRequestURI().getPath();
-		if (endpoint.equals("/game/model") && requestMethod.equals("GET")) {
-
-			String query = exchange.getRequestURI().getQuery();
-			System.out.println(query);
+		
+		System.out.println(endpoint);
+		switch (endpoint) {
+		case "/game/model" :
+			if (HandlerUtils.checkRequestMethod("GET", exchange)){ getModel(exchange); }
+			break;
+		case "/game/reset" :
+			break;
+		case "/game/commands" :
+			break;
+		case "/game/AddAI" :
+			break;
+		case "/game/listAI" :
+			break;
+		default: 
+			System.out.println("gameHandler. Endpoint: " + endpoint + ". Sent Bad Request");
+			HandlerUtils.sendEmptyBody(exchange, HttpURLConnection.HTTP_BAD_REQUEST);
+		}
+		//game/model
+		//game/reset
+		//game/commands
+		//game/AddAI
+		//game/listAI
+		
+		
+	}
+	
+	private void getModel (HttpExchange exchange) {
+		
+	}
+	
+	private boolean checkRequestMethod (String type, HttpExchange exchange) throws IOException {
+		if (exchange.getRequestMethod().toUpperCase().equals(type)) {
+			return true;
+		} else {
+			Server.println("  Bad request to /games/list." + exchange.getRequestMethod().toUpperCase());
+			HandlerUtils.sendEmptyBody(exchange, HttpURLConnection.HTTP_BAD_REQUEST);
+			return false;
 		}
 	}
 

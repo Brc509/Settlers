30e6397f7dfb44945d9f11e58e84300a6141205a
nikolashin <g.nikolashin@gmail.com>
2014-03-29 20:23:10 -0600

    build settlement updated

--- server/src/catan/server/command/moves/BuildSettlementCommand.java
+++ server/src/catan/server/command/moves/BuildSettlementCommand.java
@@ -1,9 +1,11 @@
 package catan.server.command.moves;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import catan.model.ClientModel;
+import catan.model.Hex;
 import catan.model.Model;
 import catan.model.Player;
 import catan.model.ResourceList;
@@ -13,6 +15,7 @@ import catan.server.command.Command;
 import catan.server.handler.HandlerUtils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
@@ -58,7 +61,96 @@ public class BuildSettlementCommand implements Command{
 			model.getModel().get("bank").getAsJsonObject().addProperty("wheat", wheat+1);
 		}
 		
+		JsonArray hexes = model.getModel().get("map").getAsJsonObject().get("hexGrid")
+							.getAsJsonObject().get("hexes").getAsJsonArray();
+		
+		String direction = vertexLocation.direction;
+		int corner = getCornerValue(direction);
+		
+		if(direction.equals("NE"))
+		{
+			// (x, y-1, "SE")(x+1, y-1, "W") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x, vertexLocation.y-1,"SE",hexes), getCornerValue("SE"), playerIndex);
+			setHex(getHex(vertexLocation.x+1, vertexLocation.y-1,"W",hexes), getCornerValue("W"), playerIndex);
+		}
+		if(direction.equals("NW"))
+		{
+			// (x, y-1, "SW")(x-1, y, "E") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x, vertexLocation.y-1,"SW",hexes), getCornerValue("SW"), playerIndex);
+			setHex(getHex(vertexLocation.x-11, vertexLocation.y,"E",hexes), getCornerValue("E"), playerIndex);
+		}
+		if(direction.equals("E"))
+		{
+			// (x+1, y-1, "SW")(x+1, y, "NW") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x+1, vertexLocation.y-1,"SW",hexes), getCornerValue("SW"), playerIndex);
+			setHex(getHex(vertexLocation.x+1, vertexLocation.y,"NW",hexes), getCornerValue("NW"), playerIndex);
+		}
+		if(direction.equals("SW"))
+		{
+			// (x, y+1, "NW")(x-1, y+1, "E") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x, vertexLocation.y+1,"NW",hexes), getCornerValue("NW"), playerIndex);
+			setHex(getHex(vertexLocation.x-11, vertexLocation.y+1,"E",hexes), getCornerValue("E"), playerIndex);
+		}
+		if(direction.equals("SE"))
+		{
+			// (x, y+1, "NE")(x+1, y, "W") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x, vertexLocation.y+1,"NE",hexes), getCornerValue("NE"), playerIndex);
+			setHex(getHex(vertexLocation.x+1, vertexLocation.y,"W",hexes), getCornerValue("W"), playerIndex);
+		}
+		if(direction.equals("W"))
+		{
+			// (x-1, y, "SE")(x-1, y+1, "NE") too
+			setHex(getHex(vertexLocation.x, vertexLocation.y,direction,hexes), corner, playerIndex);
+			setHex(getHex(vertexLocation.x-1, vertexLocation.y,"SE",hexes), getCornerValue("SE"), playerIndex);
+			setHex(getHex(vertexLocation.x-1, vertexLocation.y+1,"NE",hexes), getCornerValue("NE"), playerIndex);
+		}
+		
 		return null;
+	}
+	
+	public void setHex(JsonObject hex, int corner, int player)
+	{
+		hex.getAsJsonObject().get("vertexes").getAsJsonArray().get(corner)
+							.getAsJsonObject().get("value").getAsJsonObject()
+							.addProperty("worth", 1);
+		hex.getAsJsonObject().get("vertexes").getAsJsonArray().get(corner)
+							.getAsJsonObject().get("value").getAsJsonObject()
+							.addProperty("ownerID", player);
+	}
+	
+	public JsonObject getHex(int x, int y, String direction, JsonArray hexes)
+	{
+		for(int i=0;i<hexes.size();i++)
+		{
+			JsonArray setOfHexes = hexes.get(i).getAsJsonArray();
+			for(int j=0;j<setOfHexes.size();j++)
+			{
+				JsonObject hex = setOfHexes.get(j).getAsJsonObject();
+				if(hex.getAsJsonObject().get("location").getAsJsonObject().get("x").getAsInt()==x)
+					if(hex.getAsJsonObject().get("location").getAsJsonObject().get("y").getAsInt()==y)
+						return hex;
+			}
+		}
+		
+		return null;
+	}
+	
+	public int getCornerValue(String direction)
+	{
+		int corner1 = -1;
+		if(direction.equals("NE")){corner1 = 0;}
+		if(direction.equals("NW")){corner1 = 1;}
+		if(direction.equals("E")){corner1 = 2;}
+		if(direction.equals("SW")){corner1 = 3;}
+		if(direction.equals("SE")){corner1 = 4;}
+		if(direction.equals("W")){corner1 = 5;}
+		return corner1;
 	}
 
 }
+

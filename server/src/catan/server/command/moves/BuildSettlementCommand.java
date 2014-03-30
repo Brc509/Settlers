7662e3f843952884aa1350bb07e77ac5bb0a13c1
nikolashin <g.nikolashin@gmail.com>
2014-03-29 20:41:23 -0600

    build settlement fix

--- server/src/catan/server/command/moves/BuildSettlementCommand.java
+++ server/src/catan/server/command/moves/BuildSettlementCommand.java
@@ -144,11 +144,11 @@ public class BuildSettlementCommand implements Command{
 	{
 		int corner1 = -1;
 		if(direction.equals("NE")){corner1 = 0;}
-		if(direction.equals("NW")){corner1 = 1;}
-		if(direction.equals("E")){corner1 = 2;}
+		if(direction.equals("E")){corner1 = 1;}
+		if(direction.equals("SE")){corner1 = 2;}
 		if(direction.equals("SW")){corner1 = 3;}
-		if(direction.equals("SE")){corner1 = 4;}
-		if(direction.equals("W")){corner1 = 5;}
+		if(direction.equals("W")){corner1 = 4;}
+		if(direction.equals("NW")){corner1 = 5;}
 		return corner1;
 	}
 

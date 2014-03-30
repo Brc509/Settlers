cac7b65f24bc63be2b3057febb4ae9feea4af33f
Spencer Bench <spencer.bench@gmail.com>
2014-03-29 23:23:15 -0600

    BuyDevCard is complete.

--- server/src/catan/server/command/moves/BuyDevCardCommand.java
+++ server/src/catan/server/command/moves/BuyDevCardCommand.java
@@ -1,25 +1,87 @@
 package catan.server.command.moves;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import catan.model.DevCardList;
+import catan.model.Model;
+import catan.model.ResourceList;
+import catan.server.Games;
+import catan.server.Server;
 import catan.server.command.Command;
 
-import com.google.gson.JsonObject;
-import com.sun.net.httpserver.HttpExchange;
+public class BuyDevCardCommand implements Command {
 
-public class BuyDevCardCommand implements Command{
-	
-	HttpExchange arg0;
-	JsonObject json;
+	private String type;
+	private int playerIndex;
 
-	public BuyDevCardCommand(HttpExchange arg0,JsonObject json){
-		// TODO Auto-generated constructor stub
-		this.arg0 = arg0;
-		this.json = json;
-		//execute();
-	}
+	public BuyDevCardCommand() {}
 
 	@Override
 	public Object execute(Object obj) {
-		// TODO Auto-generated method stub
+
+		Server.println("Executing command: \"" + type + "\".");
+
+		// Pick a random dev card from the deck
+		int gameID = (Integer) obj;
+		Model game = Games.get().getGames().get(gameID);
+		DevCardList deck = game.getDeck();
+		List<Integer> stack = new ArrayList<>();
+		for (int n = 0; n < deck.getMonopoly(); n++) {
+			stack.add(0);
+		}
+		for (int n = 0; n < deck.getMonument(); n++) {
+			stack.add(1);
+		}
+		for (int n = 0; n < deck.getRoadBuilding(); n++) {
+			stack.add(2);
+		}
+		for (int n = 0; n < deck.getSoldier(); n++) {
+			stack.add(3);
+		}
+		for (int n = 0; n < deck.getYearOfPlenty(); n++) {
+			stack.add(4);
+		}
+		int index = new Random().nextInt(stack.size());
+		int pick = stack.get(index);
+
+		// Move the dev card from the deck to the player
+		DevCardList newDC = game.getPlayerNewDevCards(playerIndex);
+		if (pick == 0) {
+			newDC.setMonopoly(newDC.getMonopoly() + 1);
+			deck.setMonopoly(deck.getMonopoly() - 1);
+		} else if (pick == 1) {
+			newDC.setMonument(newDC.getMonument() + 1);
+			deck.setMonument(deck.getMonument() - 1);
+		} else if (pick == 2) {
+			newDC.setRoadBuilding(newDC.getRoadBuilding() + 1);
+			deck.setRoadBuilding(deck.getRoadBuilding() - 1);
+		} else if (pick == 3) {
+			newDC.setSoldier(newDC.getSoldier() + 1);
+			deck.setSoldier(deck.getSoldier() - 1);
+		} else if (pick == 4) {
+			newDC.setYearOfPlenty(newDC.getYearOfPlenty() + 1);
+			deck.setYearOfPlenty(deck.getYearOfPlenty() - 1);
+		}
+		game.setPlayerNewDevCards(playerIndex, newDC);
+		game.setDeck(deck);
+
+		// Charge the player for the card
+		ResourceList pR = game.getPlayerResources(playerIndex);
+		ResourceList bank = game.getBank();
+		pR.setOre(pR.getOre() - 1);
+		pR.setSheep(pR.getSheep() - 1);
+		pR.setWheat(pR.getWheat() - 1);
+		bank.setOre(bank.getOre() + 1);
+		bank.setSheep(bank.getSheep() + 1);
+		bank.setWheat(bank.getWheat() + 1);
+		game.setPlayerResources(playerIndex, pR);
+		game.setBank(bank);
+
+		// Add a log entry
+		game.addLogEntry(playerIndex, " bought a development card.");
+
 		return null;
 	}
 

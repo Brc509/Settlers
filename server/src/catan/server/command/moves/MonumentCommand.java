f30fb0322d5809831c854632b2b6eedcb8f064b7
Spencer Bench <spencer.bench@gmail.com>
2014-03-29 21:27:11 -0600

    Some work on Monument.

--- server/src/catan/server/command/moves/MonumentCommand.java
+++ server/src/catan/server/command/moves/MonumentCommand.java
@@ -1,19 +1,20 @@
 package catan.server.command.moves;
 
-import catan.model.Model;
-import catan.server.Games;
+import catan.server.Server;
 import catan.server.command.Command;
 
 public class MonumentCommand implements Command {
+
 	private String type;
 	private int playerIndex;
-	
+
 	@Override
 	public Object execute(Object gameId) {
-		// TODO Auto-generated method stub
-		System.out.println(type + playerIndex);
-		Model model = Games.get().getGames().get(gameId);
+
+		Server.println("Executing command: \"" + type + "\".");
+
+		// TODO
+
 		return null;
 	}
-
 }

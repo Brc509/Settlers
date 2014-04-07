c28fb16e2e323065491d17b80e3538e98bf5c04d
Skyler Mitchell <skylrmitchll@gmail.com>
2014-04-07 12:00:08 -0600

    Cleaned out old files

--- server/src/catan/z/oldCode/server/handler/moves/MovesBuildRoadHandler.java
+++
@@ -1,7 +0,0 @@
-package catan.z.oldCode.server.handler.moves;
-
-import com.sun.net.httpserver.HttpHandler;
-
-public interface MovesBuildRoadHandler extends HttpHandler {
-	
-}
--- server/src/catan/z/oldCode/server/handler/moves/MovesSoldierHandler.java
+++
@@ -1,5 +0,0 @@
-package catan.z.oldCode.server.handler.moves;
-
-import com.sun.net.httpserver.HttpHandler;
-
-public interface MovesSoldierHandler extends HttpHandler {}

667ed956421048908c585fabdcc40562bd33a00c
herrmannator05 <cole.herrmann05@gmail.com>
2014-04-08 22:15:47 -0600

    tests work pretty much

--- gameplay/js/model/tests.js
+++ gameplay/js/model/tests.js
@@ -25,14 +25,15 @@ test("model.initFromServer()", 4, function(){
 		ok(true, "Sam has logged in.");
 		start();
 		
-		stop();
 		
 		var clientModel = new catan.models.ClientModel(0);
 
-		
+		stop();
+
 		clientModel.initFromServer(function () {
 
-			ok(clientModel.players[0] && clientModel.players[1] && clientModel.players[2] && clientModel.players[3], "Initialized model with 4 players.");
+			ok(clientModel.players[0] && clientModel.players[1] && 
+				clientModel.players[2] && clientModel.players[3], "Initialized model with 4 players.");
 			ok(clientModel.turnTracker.status == "Rolling" && clientModel.turnTracker.currentTurn == 0, "Initialized turn tracker correctly.");
 			ok(clientModel.map !== undefined, "Map initialized");
 
@@ -48,34 +49,36 @@ test( "model.yearOfPlenty()", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		// start();
+		
+		// stop();
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
+			// start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
+			// stop();
 			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
+			setTimeout(function(){
+				
+				command.yearOfPlenty("wheat", "brick");
+			
+				setTimeout(function(){
+					
+					clientModel.players;
+					var player = clientModel.players[0];
+					var resources = player.resources;
+					ok(resources.brick > 0 && resources.wheat > 1, "Sam successfully executed year of plenty, proper resources were incrememnted.");
+					start();
+				
+				}, 500);
+				
 
-			stop();
-			command.yearOfPlenty("wheat", "brick");
-			start();
-			stop();
-			clientModel.initFromServer(function(){
-				clientModel.players;
-				var player = clientModel.players[0];
-				var resources = player.resources;
-				//sam's brick starts out at 0 and wheat at 1, so if we know those increased then he succesfully completed YOP
-				ok(resources.brick > 0 && resources.wheat > 1, "Sam successfully executed year of plenty, proper resources were incrememnted.");
-				start();
-
-			})
+			}, 500);
+			
 			
 		});
 
@@ -135,16 +138,12 @@ test( "clientProxy.buyDevCard()", function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Logged in");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized Sam");
-			start();
-
 
 			clientModel.clientPlayer.resources['ore'] ++;
-			stop();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
@@ -155,25 +154,21 @@ test( "clientProxy.buyDevCard()", function() {
 			clientModel.players[0].newDevCards.soldier,
 			clientModel.players[0].newDevCards.yearOfPlenty];
 
-
-			// stop();
 			command.buyDevCard();
-			start();
+
+			setTimeout(function(){
 				
-			stop();	
-			clientModel.initFromServer(function () {
-
 				var postArray = [clientModel.players[0].newDevCards.monopoly,
-			clientModel.players[0].newDevCards.monument,
-			clientModel.players[0].newDevCards.roadBuilding,
-			clientModel.players[0].newDevCards.soldier,
-			clientModel.players[0].newDevCards.yearOfPlenty];
+				clientModel.players[0].newDevCards.monument,
+				clientModel.players[0].newDevCards.roadBuilding,
+				clientModel.players[0].newDevCards.soldier,
+				clientModel.players[0].newDevCards.yearOfPlenty];
 
-				ok(true, "Sam's dev cards have changed, success.");
+				ok(preArray.toString() != postArray.toString(), "Sam's dev cards have changed, success.");
 				start();
-			});
-
 			
+			}, 3000);
+				
 		});
 	});
 
@@ -185,39 +180,29 @@ test( "clientProxy.rollNumber()", function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Logged in");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "inited Sam");
-			start();
-			
-			
+						
 			var preWheat = clientModel.clientPlayer.resources['wheat'];
-			stop();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
 			// stop();
 			command.rollNumber(6);
-			start();
-				
-			stop();	
-			clientModel.initFromServer(function () {
-
+			setTimeout(function(){
 				var postWheat = clientModel.clientPlayer.resources['wheat'];
 
-				ok(true, "Sam rolled a 6.  Good work there sammy");
-				ok(true, "Sam gained appropriate resources from roll.")
+				ok(clientModel.turnTracker.status = "Playing", "Sam rolled a 6.  Good work there sammy");
+				ok(postWheat - preWheat, "Sam gained appropriate resources from roll.")
 				start();
-			});
-
+			
+			}, 2000);	
 			
 		});
 	});
-
-	ok( 1 == "1", "Called both clientProxy methods.  Wait for callback" );
 });
 
 
@@ -227,8 +212,7 @@ test( "model.soldier(robberSpot, victimID)", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			var preArray = [clientModel.players[0].resources.wheat,
@@ -238,42 +222,30 @@ test( "model.soldier(robberSpot, victimID)", 3, function() {
 			clientModel.players[0].resources.wood];
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
-
-			stop();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
-			var command = new catan.models.Command(proxy, clientModel);
-
-			// stop();
-			command.rollNumber(6);
-			start();
-			stop();
+			var command = new catan.models.Command(proxy, clientModel);			
+			
 			command.soldier(1, new catan.models.HexLocation(1,2));
-			start();
-			stop()
-
-			clientModel.initFromServer(function () {
+			setTimeout(function(){
+				
 				clientModel.players;
 				var postArray = [clientModel.players[0].resources.wheat,
-					clientModel.players[0].resources.brick,
-					clientModel.players[0].resources.sheep,
-					clientModel.players[0].resources.ore,
-					clientModel.players[0].resources.wood];
+				clientModel.players[0].resources.brick,
+				clientModel.players[0].resources.sheep,
+				clientModel.players[0].resources.ore,
+				clientModel.players[0].resources.wood];
 
-					var a1String = preArray.toString() // "1,2,3"
-					var a2String = postArray.toString() // "1,2,3"
+				var a1String = preArray.toString() // "1,2,3"
+				var a2String = postArray.toString() // "1,2,3"
 
-					ok(a1String != a2String, "Sam's resources changed, soldier worked.");
-					start();
-			});
+				ok(a1String != a2String, "Sam's resources changed, soldier worked.");
+				start();
+
+			}, 2000)
 			
 		});
 	});
-
-
-	// var status = clientModel.soldier(1,2);
-	// ok(true == true, status);
 
 });
 
@@ -283,33 +255,24 @@ test( "model.monopoly()", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
 			command.monopoly("wheat");
-			start();
-			stop();
-			clientModel.initFromServer(function(){
+			setTimeout(function(){
+				
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
 				ok(resources.wheat > 3, "Sam successfully executed monopoly, specified resources increased.");
 				start();
-
-			})
+			
+			}, 2000)
 			
 		});
 
@@ -322,33 +285,24 @@ test( "model.monument()", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
 			command.monument();
-			start();
-			stop();
-			clientModel.initFromServer(function(){
+			setTimeout(function(){				
+				
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
 				ok(resources.wheat > 3, "Sam successfully executed monument");
 				start();
-
-			})
+			
+			}, 2000)
 			
 		});
 
@@ -362,14 +316,11 @@ test( "model.buildSettlement()", 4, function() {
 	var clientModel = new catan.models.ClientModel(0);
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
-
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
@@ -381,18 +332,11 @@ test( "model.buildSettlement()", 4, function() {
 			var sheep = resources.sheep;
 			var wheat = resources.wheat;
 
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
 			var vLocation = new catan.models.VertexLocation("NW", 1, 2);
 			
 			command.buildSettlement(vLocation, false);
-			start();
-			stop();
-			clientModel.initFromServer(function(){
+			setTimeout(function(){
+				
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
@@ -403,8 +347,8 @@ test( "model.buildSettlement()", 4, function() {
 				ok(player.settlements == 2, "Sam successfully built a settlement");
 				ok(brick - brickPost == 1 && wood - woodPost == 1, "Resources for settlement successfully decremented");
 				start();
-
-			})
+			
+			}, 2000)
 			
 		});
 
@@ -417,22 +361,14 @@ test( "model.buildCity()", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
 			var vLocation = new catan.models.VertexLocation("SW", 1, 2);
 			var player = clientModel.players[0];
 			var resources = player.resources;
@@ -444,10 +380,8 @@ test( "model.buildCity()", 3, function() {
 
 
 			command.buildCity(vLocation, false);
-			start();
-			stop();
-
-			clientModel.initFromServer(function(){
+			setTimeout(function(){
+				
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
@@ -463,7 +397,7 @@ test( "model.buildCity()", 3, function() {
 				ok(true, "Sam successfully built a city.");
 				start();
 
-			})
+			}, 2000)
 			
 		});
 
@@ -473,37 +407,33 @@ test( "model.buildCity()", 3, function() {
 test( "model.buildRoad()", 3, function() {
 	stop();
 	var clientModel = new catan.models.ClientModel(0);
-	loginPlayer("Brooke", "brooke", "blue", "0", function () {
+	loginPlayer("Sam", "sam", "red", "0", function () {
 
-		ok(true, "Brooke has logged in.");
-		start();
-		stop();
+		ok(true, "Sam has logged in.");
+		
 		clientModel.initFromServer(function () {
 
-			ok(true, "Initialized the server with Brooke as the current player.");
-			start();
+			ok(true, "Initialized the server with Sam as the current player.");
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 			var hexLocaction = new catan.models.HexLocation(1,2);
 			var edgeLocation = new catan.models.hexgrid.EdgeLocation(-1,-2,4);
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
+	
+			var player = clientModel.players[0];
+			var resources = player.resources;
+			var roads = player.roads;
 
-			stop();
 			command.buildRoad(edgeLocation, false);
-			start();
-			stop();
-			clientModel.initFromServer(function(){
-				clientModel.players;
-				var player = clientModel.players[0];
-				var resources = player.resources;
-				ok(resources.wheat > 3, "Sam successfully executed monopoly");
+			setTimeout(function(){
+				
+				var playerPost = clientModel.players[0];
+				var resourcesPost = player.resources;
+				var roadsPost = player.roads;
+				ok(roads - roadsPost == 0, "Sam successfully built a road");
 				start();
-
-			})
+			
+			}, 4000)
 			
 		});
 
@@ -516,35 +446,25 @@ test( "model.offerTrade()", 3, function() {
 	loginPlayer("Sam", "sam", "red", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Sam as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
 			var rList = new catan.models.ResourceList();
 			rList.update({brick:-5, wood:-5, sheep:1, wheat:0, ore:-9});
 			command.offerTrade(1, rList);
-			start();
-			stop();
-			clientModel.initFromServer(function(){
+			
+			setTimeout(function(){
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
-				ok(resources.wheat > 3, "Sam successfully executed monopoly");
+				ok(true, "Sam successfully offered trade");
 				start();
-
-			})
+			}, 2000)
 			
 		});
 
@@ -557,80 +477,49 @@ test( "model.acceptTrade()", 3, function() {
 	loginPlayer("Brooke", "brooke", "blue", "0", function () {
 
 		ok(true, "Brooke has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Brooke as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
-
-			stop();
-			command.rollNumber(6);
-			start(); 
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			stop();
+			
 			command.acceptTrade(true);
-			start();
-			stop();
-			clientModel.initFromServer(function(){
+			setTimeout(function(){
 				clientModel.players;
 				var player = clientModel.players[0];
 				var resources = player.resources;
-				ok(resources.wheat > 3, "Sam successfully executed monopoly");
+				ok(true, "Brooke successfully accepted trade");
 				start();
-
-			})
+			}, 2000)
 			
 		});
 
 	});
 });
 
-test( "model.finishTurn()", 4, function() {
+test( "model.finishTurn()", 3, function() {
 	stop();
 	var clientModel = new catan.models.ClientModel(0);
 	loginPlayer("Sam", "sam", "orange", "0", function () {
 
 		ok(true, "Sam has logged in.");
-		start();
-		stop();
+		
 		clientModel.initFromServer(function () {
 
 			ok(true, "Initialized the server with Brooke as the current player.");
-			start();
 
 			var proxy = new catan.models.ClientProxy(clientModel);
 			var command = new catan.models.Command(proxy, clientModel);
 
-			stop();
-			command.rollNumber(6);
-			clientModel.initFromServer(function(){
-				
-				ok(clientModel.turnTracker.status == "Playing", "Sam successfully executed monopoly");
+			command.finishTurn();
+			setTimeout(function(){
+				ok(clientModel.turnTracker.status == "Rolling", "Sam successfully finished turn");
 				start();
-
-				stop();
-				command.finishTurn();
-				start();
-				stop();
-				clientModel.initFromServer(function(){
-				
-					ok(true, "Sam successfully executed monopoly");
-					start();
-
-				})
-
-			})
-			// ok("Playing" == clientModel.turnTracker.status, "Successfully rolled a number (rolled the die)");
-
-			
+			}, 2000)
 			
 		});
-
 	});
 });
 // test( "model.canDiscardCards()", function() {

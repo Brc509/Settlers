cd063a9d1072ac16bc6a894f0e62bf77cff3a2cf
herrmannator05 <cole.herrmann05@gmail.com>
2014-04-14 12:00:04 -0600

    sql stuff rad

--- server/src/catan/server/persistence/SQLitePlugin.java
+++ server/src/catan/server/persistence/SQLitePlugin.java
@@ -2,6 +2,8 @@ package catan.server.persistence;
 
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;  
 import java.sql.DriverManager;  
 import java.sql.PreparedStatement;
@@ -14,73 +16,84 @@ import catan.server.RegisteredUser;
 import catan.server.command.Command;
 
 public class SQLitePlugin implements PersistenceProvider {
+	
+	private static Connection conn;
 
 	public SQLitePlugin() {
-		Connection conn = null;
-	    Statement stmt1 = null;
+
+		Statement stmt1 = null;
 	    Statement stmt2 = null;
 	    Statement stmt3 = null;
 	    Statement stmt4 = null;
-
-		 	
+	
 	    try {
 			
 		 	Class.forName("org.sqlite.JDBC");
-			
-	        conn = DriverManager.getConnection("jdbc:sqlite:server/persistence/sqlite/catan.sqlite");
-	        
-	        stmt1 = conn.createStatement();
-	        String gameTable = "CREATE TABLE Game("
-	        				+"Id INTEGER NOT NULL, "
-	        				+"GameModel BLOB NOT NULL, "
-	        				+"OrigGameModel BLOB NOT NULL, "
-	        				+"LastSavedGame INTEGER NOT NULL, "
-	        				+"PRIMARY KEY(Id)"
-	        				+");"; 
-	        
-	        stmt1.executeUpdate(gameTable);
-	        stmt1.close();
-
-	        stmt2 = conn.createStatement();
-	        String userTable = "CREATE TABLE User("
-    				+"Id INTEGER NOT NULL, "
-    				+"Name varchar(255) NOT NULL, "
-    				+"Password varchar(255) NOT NULL, "
-    				+"PRIMARY KEY(Id)"
-    				+");"; 
-	        stmt2.executeUpdate(userTable);
-	        stmt2.close();
-	        
-	        stmt3 = conn.createStatement();
-	        String commandTable = "CREATE TABLE Command("
-    				+"Id INTEGER NOT NULL, "
-    				+"Command BLOB NOT NULL, "
-    				+"GameID INTEGER NOT NULL, "
-    				+"PRIMARY KEY(Id), "
-    				+"FOREIGN KEY(GameId) REFERENCES Game(Id)"
-    				+");"; 
-	        
-	        stmt3.executeUpdate(commandTable);
-	        stmt3.close();
-	        
-	        stmt4 = conn.createStatement();
-	        String activityTable = "CREATE TABLE Activity("
-    				+"UserId INTEGER NOT NULL, "
-    				+"GameId INTEGER NOT NULL, "
-    				+"FOREIGN KEY(UserId) REFERENCES User(Id)"
-    				+"FOREIGN KEY(GameId) REFERENCES Game(Id)"
-    				+");"; 
-	        
-	        stmt4.executeUpdate(activityTable);
-	        stmt4.close();
-	        
-	        conn.close();
-	        
-		 	} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e){
-				e.printStackTrace();
+			File file = new File("server/persistence/sqlite/catan.sqlite");
+			if(!file.exists()){
+				file.getParentFile().mkdirs();
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
+	        
+			conn = DriverManager.getConnection("jdbc:sqlite:" + file.getPath());
+	        
+	        if(file.length() == 0){
+		        stmt1 = conn.createStatement();
+		        String gameTable = "CREATE TABLE Game("
+		        				+"Id INTEGER NOT NULL, "
+		        				+"GameModel BLOB NOT NULL, "
+		        				+"OrigGameModel BLOB NOT NULL, "
+		        				+"LastSavedGame INTEGER NOT NULL, "
+		        				+"PRIMARY KEY(Id)"
+		        				+");"; 
+		        
+		        stmt1.executeUpdate(gameTable);
+		        stmt1.close();
+	
+		        stmt2 = conn.createStatement();
+		        String userTable = "CREATE TABLE User("
+	    				+"Id INTEGER NOT NULL, "
+	    				+"Name varchar(255) NOT NULL, "
+	    				+"Password varchar(255) NOT NULL, "
+	    				+"PRIMARY KEY(Id)"
+	    				+");"; 
+		        stmt2.executeUpdate(userTable);
+		        stmt2.close();
+		        
+		        stmt3 = conn.createStatement();
+		        String commandTable = "CREATE TABLE Command("
+	    				+"Id INTEGER NOT NULL, "
+	    				+"Command BLOB NOT NULL, "
+	    				+"GameID INTEGER NOT NULL, "
+	    				+"PRIMARY KEY(Id), "
+	    				+"FOREIGN KEY(GameId) REFERENCES Game(Id)"
+	    				+");"; 
+		        
+		        stmt3.executeUpdate(commandTable);
+		        stmt3.close();
+		        
+		        stmt4 = conn.createStatement();
+		        String activityTable = "CREATE TABLE Activity("
+	    				+"UserId INTEGER NOT NULL, "
+	    				+"GameId INTEGER NOT NULL, "
+	    				+"FOREIGN KEY(UserId) REFERENCES User(Id)"
+	    				+"FOREIGN KEY(GameId) REFERENCES Game(Id)"
+	    				+");"; 
+		        
+		        stmt4.executeUpdate(activityTable);
+		        stmt4.close();
+		        
+		        }
+		        
+			 	} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (SQLException e){
+					e.printStackTrace();
+				}
 	    }
 	
 	@Override
